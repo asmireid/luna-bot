ede596cb90600eefde6e53c11bfa0515fb08023a
Zihan Wang <jameszh.wang@mail.utoronto.ca>
2026-01-09 18:32:20 -0500

    fix the issue that set reload is not triggered

--- cogs/set_config.py
+++ cogs/set_config.py
@@ -27,6 +27,7 @@ PAINT_RELOAD_KEYS = {
 
 
 async def set_helper(ctx, option, value: str):
+    print("Set Helper Triggered!")
     configs = Config()
 
     # Check if the specified option exists as an attribute of the Config instance
@@ -45,10 +46,10 @@ async def set_helper(ctx, option, value: str):
                                 title=f"{configs.bot_name}'s State",
                                 descr=f"{option} is updated.")
         conf_embed.add_field(name="Old -> New", value=f"{old_value} -> {value}")
-        await try_display_confirmation(ctx, conf_embed)
-
         await _reload_chat_backend_if_needed(ctx, option)
         await _reload_paint_backend_if_needed(ctx, option)
+        
+        await try_display_confirmation(ctx, conf_embed)
     else:
         await try_reply(ctx, f"{option} is not a valid option...")
 
@@ -86,6 +87,7 @@ async def _reload_chat_backend_if_needed(ctx, changed_option):
 
 
 async def _reload_paint_backend_if_needed(ctx, changed_option):
+    print(changed_option)
     if changed_option not in PAINT_RELOAD_KEYS:
         return
 
@@ -133,7 +135,7 @@ class SetConfig(commands.Cog):
             'command_prefix': command_prefix,
             'prefix': command_prefix
         }
-
+        print(option, value)
         set_function = switch_options.get(option, None)
 
         if set_function is not None:
